1e1a3e46e6a0390c9b10bf7c0d9087b7093932cd
Paul Jones <shmiko@gmail.com>
2016-03-03 19:49:01 +1100

    new gem

--- db/migrate/20150125212019_add_category_subcategory_to_listings.rb
+++
@@ -1,6 +0,0 @@
-class AddCategorySubcategoryToListings < ActiveRecord::Migration
-  def change
-  	add_column :listings, :category_id, :integer
-   	add_column :listings, :subcategory_id, :integer
-  end
-end
